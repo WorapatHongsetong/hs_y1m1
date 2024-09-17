b305d4f46c8b2bcbb03db342eb2d27a27b888c21
WorapatHongsetong <worapath154@gmail.com>
2024-09-17 20:28:28 +0700

    String is a sequence of charaters

--- final_project.py
+++ final_project.py
@@ -2,3 +2,15 @@
 """
 I gonna make a burger making game but not limited to burgers :D
 """
+
+"""
+# Approved Lib
+    # math
+    # random
+    # time
+    # keyboard
+
+# Clear Screen
+    import os
+    os.system('cls' if os.name == 'nt' else 'clear')
+"""
