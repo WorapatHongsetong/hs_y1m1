d46470c10e3ced45e3af7d559162bc470ce773f0
WorapatHongsetong <worapath154@gmail.com>
2024-09-18 22:28:07 +0700

    Map allowed key to Dict(base10)

--- final_project.py
+++ final_project.py
@@ -1,8 +1,3 @@
-# Concept
-"""
-I gonna make a burger making game but not limited to burgers :D
-"""
-
 """
 # Approved Lib
     # math
@@ -14,3 +9,18 @@ I gonna make a burger making game but not limited to burgers :D
     import os
     os.system('cls' if os.name == 'nt' else 'clear')
 """
+
+
+char_mapping = {}
+
+# Mapping digits 0-9
+for i in range(10):
+    char_mapping[str(i)] = i
+
+# Mapping letters a-z
+for i, letter in enumerate('abcdefghijklmnopqrstuvwxyz'):
+    char_mapping[letter] = 10 + i
+
+# Mapping special characters '.' and '@'
+char_mapping['.'] = 36
+char_mapping['@'] = 37
